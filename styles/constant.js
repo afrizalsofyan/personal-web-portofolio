69560b9086fcbf58235c1b545d7b1867473d24c3
rizalsofyan <afrizalafaandy@gmail.com>
2022-09-26 19:44:37 +0000

    fixing feature

---
+++ styles/constant.js
@@ -0,0 +1,14 @@
+export const nav_style = (bgTransparent, paddingNav, boxShaddowNav, colorText) => (
+    {
+      background: `rgba(255, 255, 255, ${bgTransparent})`,
+      padding: `${paddingNav}px 0px`,
+      boxShadow: `rgb(0 0 0 / ${boxShaddowNav}) 0px 0px 20px 6px`,
+      color: colorText < 0.2 ? 'white' : `rgba(39, 215, 245, ${colorText})`,
+    }
+)
+
+export const logo_style = (colorText) => (
+    {
+      'border-bottom': `2px solid ${colorText < 0.2 ? 'white' : `rgba(39, 215, 245, ${colorText})`}`
+    }
+)
